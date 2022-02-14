12aa0437728ce72f2ae032b8bfb058261e876130
Agustin Mansilla <mansillaagustin1019@gmail.com>
2022-02-13 23:21:15 -0300

    API Structure

---
+++ Catalog-API/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Catalog_API.Models;
+using Catalog_API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+    }
+}
---
+++ Catalog-API/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,27 @@
+﻿using Catalog_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catalog_API.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 17, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+
+        }
+    }
+}
